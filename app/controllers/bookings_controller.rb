cbd2406404ce7df69055d3e983cb78fb691222c6
Rodolphe Desobry <rodolphe.desobry@grenoble-em.com>
2019-02-26 18:22:29 +0100

    Small progress on Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        @booking = Booking.new(params_booking)
+        # if @booking.save
+        #   PAYMENT PATH 
+        # else 
+        #   Back to booking create view 
+        # end 
+    end 
+
+    private 
+
+    def params_booking
+        params.require(:booking).permit(:artist_id, :profile_id, :date)
+    end
 end
