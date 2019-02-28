575c8f13d6af8f8387bc7b1657c74d86c436bd90
theo watine <watinetheo@gmail.com>
2019-02-28 11:24:35 +0100

    schedule removed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :artist, optional: true 
+  belongs_to :artist, optional: true
   belongs_to :profile, optional: true 
   has_many :reviews
-  has_one :schedule, :through => :artists
 end
