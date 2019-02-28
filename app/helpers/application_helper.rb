282a3b888455e64e9c2bcd0226349bf46a47f4f3
Dimitri Prouvost <dimitriprouvost@hotmail.com>
2019-02-28 18:45:34 +0100

    artist_show_5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def youtube_video(url)
     render :partial => 'artists/video', :locals => { :url => url }
   end 
+
+  def artist_avatar(url)
+    render :partial => 'artists/photo', :locals => { :url => url }
+  end
 end
