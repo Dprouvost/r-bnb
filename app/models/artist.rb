26c3fccfa143f8d74efff3e124276f450e3f24c9
Rodolphe Desobry <rodolphe.desobry@grenoble-em.com>
2019-02-28 18:45:35 +0100

    WIP

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,10 @@ class Artist < ApplicationRecord
   has_many :reviews, :through => :bookings
   belongs_to :category, optional: true 
   validates :band_name, :photo_url, :music_url, :price, :location, :description, :area_range, presence: true
+  include PgSearch
+  pg_search_scope :search_by_category,
+    against: [ :category ],
+    using: {
+      tsearch: { prefix: true } #Search GEM 
+    }
 end
