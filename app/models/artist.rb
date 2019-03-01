9310c9da9922b32054b3ad82b334fdaacc0111a9
Dimitri Prouvost <dimitriprouvost@hotmail.com>
2019-03-01 14:24:56 +0100

    alert and seed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   belongs_to :review, optional: true
   belongs_to :category, optional: true
   validates :band_name, :photo_url, :music_url, :price, :location, :description, :area_range, presence: true
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   USER_TYPE = ["Buyer", "Seller"]
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :first_name, :last_name, presence: true
   belongs_to :user
 end
