241aa755c9f8677411fb17b1555a3b60dc323f7e
Dimitri Prouvost <dimitriprouvost@hotmail.com>
2019-03-01 13:38:38 +0100

    front dimitri

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     devise_for :users
     root to: 'pages#home'
+    get 'dashboard', to: 'pages#dashboard'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :profiles, only: [ :new, :create, :show, :edit, :update ]
     resources :artists, only: [ :index, :new, :create, :show, :edit, :update, :destroy] do
