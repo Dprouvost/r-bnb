cd2c2f376a19d8936dda3d3c4d8f971bb99591a5
Rodolphe Desobry <rodolphe.desobry@grenoble-em.com>
2019-03-01 11:19:56 +0100

    Seed fixed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
   has_many :bookings
-  belongs_to :review
+  belongs_to :review, optional: true
   belongs_to :category, optional: true
   validates :band_name, :photo_url, :music_url, :price, :location, :description, :area_range, presence: true
   include PgSearch
